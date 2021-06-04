cd4fad5b2c97d3d64364fcde8477f5d20530acf3
Amrit Shokar <ashokar@tveon.com>
2021-06-03 22:42:31 -0700

    Removed old front-end folder

--- front-end/src/models/Feed.tsx
+++
@@ -1,9 +0,0 @@
-export default interface IFeed {
-    date: Date,
-    country: string,
-    city: string,
-    address: string,
-    feedType: string,
-    feedAmount: number,
-    numberOfDucks: number
-}
