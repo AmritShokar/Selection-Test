a8aa4e6ff471f5ea4d830513c5b101985970e707
Amrit Shokar <ashokar@tveon.com>
2021-06-04 16:37:51 -0700

    Renamed model

--- server/src/models/FeedEntry.ts
+++ server/src/models/FeedEntry.ts
@@ -1,7 +1,7 @@
 import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
 
 @Entity()
-export class Submission {
+export class FeedEntry {
     
     @PrimaryGeneratedColumn()
     public id: number;
