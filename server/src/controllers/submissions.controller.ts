c1290b532ffc6a7c222852f52b5e5c8b1a797842
Amrit Shokar <ashokar@tveon.com>
2021-06-04 00:10:18 -0700

    Added submissions controller

---
+++ server/src/controllers/submissions.controller.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from "express";
+
+export default class SubmissionsController {
+
+    constructor() {
+
+    }
+
+    async postEntry(req: Request, res: Response): Promise<void> {
+        
+    }
+}
--- server/src/routes/submissions.routes.ts
+++ server/src/routes/submissions.routes.ts
@@ -1,16 +1,23 @@
 import { Router } from "express";
 
+import SubmissionsController from "../controllers/submissions.controller";
+
 export default class SubmissionsRoute {
     path = process.env.API_VERSION+"/submissions";
     router: Router;
+    controller: SubmissionsController;
     
-    constructor() {
+    constructor(controller: SubmissionsController) {
         this.router = Router();
+        this.controller = controller;
         this.addRoutes();
     }
 
     private addRoutes() {
-        this.router.post(this.path + "/entry");
+        this.router.post(
+            this.path + "/entry", 
+            this.controller.postEntry.bind(this.controller)
+        );
     }
 
     getRouter(): Router {
