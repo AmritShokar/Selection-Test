0e1159085cc507c7ee390f1326bf0d07e8c6e7ea
Amrit Shokar <ashokar@tveon.com>
2021-06-04 22:11:46 -0700

    Added constraints to Feed Entry entity

--- server/src/models/FeedEntry.ts
+++ server/src/models/FeedEntry.ts
@@ -1,4 +1,5 @@
 import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { Length, IsInt, Min, Max } from "class-validator";
 
 @Entity()
 export class FeedEntry {
@@ -10,16 +11,30 @@ export class FeedEntry {
     public feedDate: Date;
 
     @Column()
+    @Length(2, 50)
     public country: string;
+
     @Column()
+    @Length(2, 50)
     public city: string;
+
     @Column()
+    @Length(2, 50)
     public address: string;
 
     @Column()
+    @Length(2, 30)
     public feedType: string;
+
     @Column()
+    @IsInt()
+    @Min(1)
+    @Max(999999)
     public feedAmount: number;
+
     @Column()
+    @IsInt()
+    @Min(1)
+    @Max(999)
     public numDucks: number;
 }
