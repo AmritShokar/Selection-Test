bbcdc2dea22adb104b7fa3da9e2680b81ba9b934
Amrit Shokar <ashokar@tveon.com>
2021-06-04 18:04:57 -0700

    Added landing image and headings

--- client/src/components/Landing.tsx
+++ client/src/components/Landing.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Grid, createStyles, makeStyles, Theme } from '@material-ui/core';
+import { Box, Grid, Button, createStyles, makeStyles, Theme, Typography } from '@material-ui/core';
 
 import DataForm from './DataForm';
 
@@ -8,10 +8,28 @@ import landing from '../res/duck-landing-image.jpg';
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     background: {
-        height: '100vh',
+        height: '60vh',
         backgroundImage: `url(${landing})`,
         backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat'
+        backgroundRepeat: 'no-repeat',
+        marginTop: '8em'
+    },
+    grid: {
+        paddingTop: '5em',
+        paddingLeft: '5em'
+    },
+    heading: {
+        color: 'white',
+        fontFamily: 'Roboto'
+    },
+    subHeading: {
+        color: 'white',
+        fontFamily: 'Roboto',
+        marginTop: '1em'
+    },
+    formButton: {
+        marginTop: '8em',
+        marginLeft: '17em'
     }
   })
 );
@@ -25,12 +43,20 @@ const Landing = () => {
     }
 
     return (
-        <div className={classes.background}>
-            <Grid>
-                <button onClick={() => { setIsDialogOpen(!isDialogOpen) }}>Test</button>
+        <Box className={classes.background}>
+            <Grid container className={classes.grid}>
+                <Grid item xs={12}>
+                    <Typography variant="h2" className={classes.heading}>We need your help!</Typography>
+                </Grid>
+                <Grid item xs={12}>
+                    <Typography variant="h5" className={classes.subHeading}>
+                        Take a minute to help us better understand how ducks are fed
+                    </Typography>
+                </Grid>
+                <Button variant="contained" className={classes.formButton} onClick={() => { setIsDialogOpen(!isDialogOpen) }}>Help Us Out!</Button>
                 <DataForm open={isDialogOpen} toggle={toggleDialog}/>
             </Grid>
-        </div>
+        </Box>
     )
 }
 
